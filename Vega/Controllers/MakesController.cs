944fe11fc0c7caa556277a3faa53df08d35725aa
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-08-29 19:50:28 +0300

    HttpGet implementation in MakesController, Resource classes in data for automapper

---
+++ Vega/Controllers/MakesController.cs
@@ -0,0 +1,30 @@
+﻿using Data.Context;
+using Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MakesController : ControllerBase
+    {
+        private readonly VegaDbContext _context;
+
+        public MakesController(VegaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await _context.Makes.Include(m => m.Models).ToListAsync();
+        }
+    }
+}
