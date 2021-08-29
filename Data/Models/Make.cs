837bfa316553ce8bb328965e1cd0229bcec76fa3
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-08-29 03:10:43 +0300

    added new Project Data with Models

---
+++ Data/Models/Make.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Data.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+}
---
+++ Data/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
+    }
+}
