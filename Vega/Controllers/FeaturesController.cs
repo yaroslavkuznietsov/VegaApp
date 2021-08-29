43b6e4ab0defd98d94bab177f2fd9b6f7c9ca7f2
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-08-30 02:35:22 +0300

    Added FeaturesController

---
+++ Vega/Controllers/FeaturesController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Data.Context;
+using Data.Models;
+using Data.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public FeaturesController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("/api/features")]
+        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        {
+            var features = await _context.Features.ToListAsync();
+            IEnumerable<FeatureResource> featureResources = _mapper.Map<List<Feature>, List<FeatureResource>> (features);
+            return featureResources;
+        }
+    }
+}
