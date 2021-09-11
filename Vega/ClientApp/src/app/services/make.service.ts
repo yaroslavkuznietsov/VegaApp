36307b3f84810094ae40ac2e8bf972daa4ce9dc9
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-09-11 17:46:30 +0300

    added make.service.ts

---
+++ Vega/ClientApp/src/app/services/make.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MakeService {
+
+  constructor(private http: Http) { }
+
+  getMakes() {
+    return this.http.get('/api/makes').pipe(map(res => res.json())
+  }
+}
